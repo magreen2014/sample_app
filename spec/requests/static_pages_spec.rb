15d46628e7116643980bd3d221e67d2a9780272a
Matthew Green <magreen2014@gmail.com>
2014-02-25 23:23:49 -0500

    Cleaned up static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -23,7 +25,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end
@@ -36,7 +38,7 @@ describe "About page" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
   end
@@ -49,7 +51,7 @@ describe "Contact page" do
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Info")
+      expect(page).to have_title("#{base_title} | Contact Info")
     end
 
   end
